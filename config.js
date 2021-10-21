f155b183d3d1ca07203e2afd404a17b42e033c48
Veronica Adler <vlynnadler@gmail.com>
2021-10-21 09:39:34 -0400

    fixed config.js issue

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '6350249265017545',
-        clientSecret: '475c0f35280408b0c13eafd85eb0ddc1'
-    }
-}
